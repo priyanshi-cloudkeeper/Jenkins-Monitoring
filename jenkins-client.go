65e801d692936a7ec8c3faa245b2ccdc2865ce81
ck.diya.khandelwal <diya.khandelwal@cloudkeeper.com>
2025-05-19 14:41:12 +0530

    Created: DashBoard

---
+++ jenkins-client.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+    "encoding/json"
+    "fmt"
+    "io"
+    "net/http"
+)
+
+type JenkinsJob struct {
+    Name  string `json:"name"`
+    URL   string `json:"url"`
+    Color string `json:"color"`
+}
+
+type JenkinsResponse struct {
+    Jobs []JenkinsJob `json:"jobs"`
+}
+
+func fetchJobs() ([]JenkinsJob, error) {
+    url := "http://localhost:8080/api/json"
+    req, _ := http.NewRequest("GET", url, nil)
+    req.SetBasicAuth("diya", "diya")
+    client := &http.Client{}
+    resp, err := client.Do(req)
+    if err != nil {
+        return nil, err
+    }
+    defer resp.Body.Close()
+    var jenkins JenkinsResponse
+    err = json.NewDecoder(resp.Body).Decode(&jenkins)
+    return jenkins.Jobs, err
+}
+
+func jobsHandler(w http.ResponseWriter, r *http.Request) {
+    jobs, err := fetchJobs()
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+
+    var response []struct {
+        Name   string `json:"name"`
+        Status string `json:"status"`
+        URL    string `json:"url"`
+    }
+
+    for _, job := range jobs {
+        status := ""
+        switch job.Color {
+        case "blue":
+            status = "SUCCESS"
+        case "red":
+            status = "FAILURE"
+        case "blue_anime", "red_anime":
+            status = "RUNNING"
+        default:
+            status = "DISABLED"
+        }
+        response = append(response, struct {
+            Name   string `json:"name"`
+            Status string `json:"status"`
+            URL    string `json:"url"`
+        }{
+            Name:   job.Name,
+            Status: status,
+            URL:    job.URL,
+        })
+    }
+
+    w.Header().Set("Content-Type", "application/json")
+    json.NewEncoder(w).Encode(response)
+}
+
+func jobDetailHandler(w http.ResponseWriter, r *http.Request) {
+    name := r.URL.Query().Get("name")
+    if name == "" {
+        http.Error(w, "Missing job name", http.StatusBadRequest)
+        return
+    }
+    url := fmt.Sprintf("http://localhost:8080/job/%s/api/json", name)
+    req, _ := http.NewRequest("GET", url, nil)
+    req.SetBasicAuth("diya", "diya")
+    client := &http.Client{}
+    resp, err := client.Do(req)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+    defer resp.Body.Close()
+
+    w.Header().Set("Content-Type", "application/json")
+    _, err = io.Copy(w, resp.Body)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+    }
+}
--- main.go
+++ main.go
@@ -1,80 +1,18 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 
-type Build struct {
-	Class  string `json:"_class"`
-	Number int    `json:"number"`
-	URL    string `json:"url"`
-}
-
-type HealthReport struct {
-	Description   string `json:"description"`
-	IconClassName string `json:"iconClassName"`
-	IconURL       string `json:"iconUrl"`
-	Score         int    `json:"score"`
-}
-
-type JobDetail struct {
-	DisplayName           string         `json:"displayName"`
-	FullDisplayName       string         `json:"fullDisplayName"`
-	Name                  string         `json:"name"`
-	URL                   string         `json:"url"`
-	Buildable             bool           `json:"buildable"`
-	Color                 string         `json:"color"`
-	Builds                []Build        `json:"builds"`
-	FirstBuild            *Build         `json:"firstBuild"`
-	HealthReport          []HealthReport `json:"healthReport"`
-	InQueue               bool           `json:"inQueue"`
-	KeepDependencies      bool           `json:"keepDependencies"`
-	LastBuild             *Build         `json:"lastBuild"`
-	LastCompletedBuild    *Build         `json:"lastCompletedBuild"`
-	LastFailedBuild       *Build         `json:"lastFailedBuild"`
-	LastStableBuild       *Build         `json:"lastStableBuild"`
-	LastSuccessfulBuild   *Build         `json:"lastSuccessfulBuild"`
-	LastUnstableBuild     *Build         `json:"lastUnstableBuild"`
-	LastUnsuccessfulBuild *Build         `json:"lastUnsuccessfulBuild"`
-	NextBuildNumber       int            `json:"nextBuildNumber"`
-}
-
-func jobHandler(w http.ResponseWriter, r *http.Request) {
-	url := "http://localhost:8080/job/Test-Job/api/json"
-
-	client := &http.Client{}
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	req.SetBasicAuth("diya", "diya")
-
-	resp, err := client.Do(req)
-	if err != nil {
-		http.Error(w, "Error fetching job JSON: "+err.Error(), http.StatusInternalServerError)
-		return
-	}
-	defer resp.Body.Close()
-
-	var job JobDetail
-	if err := json.NewDecoder(resp.Body).Decode(&job); err != nil {
-		http.Error(w, "Error decoding JSON: "+err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	// Allow frontend access (CORS)
-	w.Header().Set("Content-Type", "application/json")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	json.NewEncoder(w).Encode(job)
-}
-
 func main() {
-	http.HandleFunc("/api/job", jobHandler)
-	http.Handle("/", http.FileServer(http.Dir("./static")))
-	fmt.Println("Server started at http://localhost:9090")
+	http.HandleFunc("/api/jobs", jobsHandler)
+	http.HandleFunc("/api/job", jobDetailHandler)
+
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fs)
+
+	fmt.Println("Server running at http://localhost:9090")
 	log.Fatal(http.ListenAndServe(":9090", nil))
 }
